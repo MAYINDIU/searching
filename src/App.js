d38cd1dc2f1926807efa586bd04fdaa1e3ea3026
unknown <mayindiu@gmail.com>
2023-10-04 01:02:39 +0600

    searching test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react'
+import TextField from '@material-ui/core/TextField';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
+ 
 function App() {
+  const filterOptions = createFilterOptions({
+    matchFrom: 'start',
+    stringify: option => option,
+  });
+ 
+  // Sample options for search box
+  const myOptions = ['Shirt', 'Shirt for man','shirt for women', 'pant',
+    'pant for men', 'Women dress', 'Dress',
+    'lungi', 'lungi for burhan'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ marginLeft: '40%', marginTop: '60px' }}>
+    <h3>Greetings from GeeksforGeeks!</h3>
+    <Autocomplete
+      style={{ width: 500 }}
+      freeSolo
+      filterOptions={filterOptions}
+      options={myOptions}
+      renderInput={(params) => (
+        <TextField {...params}
+          variant="outlined"
+          label="Search Box"
+        />
+      )}
+    />
+  </div>
   );
 }
 
